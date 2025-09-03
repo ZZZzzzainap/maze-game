899010e39f0e75cea4d66dea45de0c85077a2d5b
zainap shaapan tayea <zainapshaapan@gmail.com>
2025-09-03 21:10:16 +0300

    Add files via upload

---
+++ player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_oop_project
+{
+    internal class player : mazeopject
+    {
+        public char icon { get => '@'; }
+        public bool issolid { get => true; }
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
