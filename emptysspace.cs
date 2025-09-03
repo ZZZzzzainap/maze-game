51d716b1a05050fc0cdad277cbeb103edbf377ea
zainap shaapan tayea <zainapshaapan@gmail.com>
2025-09-03 21:04:52 +0300

    Add files via upload

---
+++ emptysspace.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_oop_project
+{
+    internal class emptysspace : mazeopject
+    {
+        public char icon { get => ' '; }
+        public bool issolid { get => false;  }
+    }
+}
