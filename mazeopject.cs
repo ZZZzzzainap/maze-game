da269e7ceb8ea0c90710c38b839bf3160b1d40df
zainap shaapan tayea <zainapshaapan@gmail.com>
2025-09-03 20:58:37 +0300

    Add files via upload

---
+++ mazeopject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_oop_project
+{
+   public interface mazeopject
+    {
+        char icon // the shape of mazeobject
+        {
+            get;
+        }
+          
+        bool issolid { get;  }// movement block
+    }
+}
