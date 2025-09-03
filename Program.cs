cebd4181cc4b61bf7e8aa9cb635737f31dbb55c4
zainap shaapan tayea <zainapshaapan@gmail.com>
2025-09-03 21:07:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace first_oop_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello to maze game!!");
+            Console.WriteLine("use the keyboardarrows to move the player");
+            maze imaze = new maze(20,50);
+            while (true)
+            {
+                imaze.drowmaze();
+                imaze.moveplayer();
+            }
+        }
+    }
+}
